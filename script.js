cfeaeed137317275810e032d41096ee59dcc0c2f
Katheryne999 <fenix.kate1985@gmail.com>
2022-04-28 21:50:27 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const NumberValueFirst = prompt ('first number');
+const NumberValueSecond = prompt ('second number');
+console. log(typeof NumberValueFirst);
+console. log(typeof NumberValueSecond);
+const sum = `&{NumberValueFirst} + &{NumberValueSecond}`;
+alert ('sum' + sum);
+console. log('sum' + sum);
+const diff = NumberValueFirst - NumberValueSecond;
+alert ('diff' + diff);
+console. log('diff' + diff); 
+const mult = NumberValueFirst * NumberValueSecond;
+alert ('mult' + mult);
+console. log('mult' + mult);
+const div = NumberValueFirst / NumberValueSecond;
+alert ('div' + div);
+console. log('div' + div);
+
+
+
+
+
+
+
+
